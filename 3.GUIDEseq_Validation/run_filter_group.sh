48b006536535e45274e9ce26c872afa9f9925221
Xiaoguo-Zheng <zhengxiaoguo@sjtu.edu.cn>
2026-03-28 14:32:30 +0800

    Update run_filter_group.sh

--- 3.GUIDEseq_Validation/run_filter_group.sh
+++ 3.GUIDEseq_Validation/run_filter_group.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 1. Define global variables and paths
-INFO_FILE="./TDOscanner/samples_for_dsODN_scanner_analsysis.txt"
-CONTROL_FILE1="./guideseq_result/oligo_control_GUIDEseq-BK_68/scanner/oligo_control_GUIDEseq-BK_68.all_dsODN.tsv"
-CONTROL_FILE2="./guideseq_result/oligo-only_control_sample-BK-92/scanner/oligo-only_control_sample-BK-92.all_dsODN.tsv"
+INFO_FILE="../3.GUIDEseq_Validation/samples_for_dsODN_scanner_analsysis.txt"
+CONTROL_FILE1="./2.guideseq_analysis/oligo_control_GUIDEseq-BK_68/scanner/oligo_control_GUIDEseq-BK_68.all_dsODN.tsv"
+CONTROL_FILE2="./2.guideseq_analysis/oligo-only_control_sample-BK-92/scanner/oligo-only_control_sample-BK-92.all_dsODN.tsv"
 
 OUT_DIR="./4.Filtered_Clean_dsODN"
 
@@ -19,10 +19,10 @@ for srr in "${GROUP1_SRRS[@]}"; do
     echo "Processing Sample: ${sample_name} ($srr)"
 
     # 【核心修改】：把 \\ 改成了 \
-    python ./TDOscanner/filter_single_background.py \
+    python ../3.GUIDEseq_Validation/filter_single_background.py \
         -s "$INFO_FILE" \
         -n "$sample_name" \
-        -t "./guideseq_result/${sample_name}/scanner/${sample_name}.all_dsODN.tsv" \
+        -t "./2.guideseq_analysis/${sample_name}/scanner/${sample_name}.all_dsODN.tsv" \
         -c "$CONTROL_FILE1" \
         -o "${OUT_DIR}/${sample_name}_Filtered_Clean.tsv"
 done
@@ -33,10 +33,10 @@ for srr in "${GROUP2_SRRS[@]}"; do
     echo "Processing Sample: ${sample_name} ($srr)"
 
     # 【核心修改】：把 \\ 改成了 \
-    python ./TDOscanner/filter_single_background.py \
+    python ../3.GUIDEseq_Validation/filter_single_background.py \
         -s "$INFO_FILE" \
         -n "$sample_name" \
-        -t "./guideseq_result/${sample_name}/scanner/${sample_name}.all_dsODN.tsv" \
+        -t "./2.guideseq_analysis/${sample_name}/scanner/${sample_name}.all_dsODN.tsv" \
         -c "$CONTROL_FILE2" \
         -o "${OUT_DIR}/${sample_name}_Filtered_Clean.tsv"
 done
